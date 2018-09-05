18d8762421105f36aac23b14b54f1c0172d9a52a
Shanise Barona <shanisebarona@gmail.com>
2018-09-05 23:36:03 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,14 +3,6 @@ module Memorable
   def reset_all
     self.all.clear
   end
-
-  # def count
-  #   @@artists.count
-  # end
-  
-  # def reset_all
-  #   @@songs.clear
-  # end
   
   def count
     self.all.count
