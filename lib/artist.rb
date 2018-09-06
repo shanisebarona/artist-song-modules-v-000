0d4b878d20e73f518877b76ab8f06a8e96316603
Shanise Barona <shanisebarona@gmail.com>
2018-09-06 00:04:22 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,12 +11,8 @@ class Artist
 
   @@artists = []
 
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
-
   def initialize
-    @@artists << self
+    self.class.all << self
     @songs = []
   end
 
